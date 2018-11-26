fecad959fbfb23f22cc4be40103bb8abfff30068
Brandon Teich <brandonteich123@gmail.com>
2018-11-26 06:47:33 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,18 +1,22 @@
 class Artist
-  attr_accessor :name, :songs
+  attr_accessor :name
+
   def initialize(name)
-      @name = name
-      @songs = []
+    @name = name
+    @songs = []
   end
+
   def add_song(song)
-      @songs << song
-      song.artist = self
+    @songs << song
+    song.artist = self
   end
+
   def songs
-      @songs
+    @songs
   end
-   def genres
-    self.songs.collect do |song| 
+
+  def genres
+    @songs.collect do |song|
       song.genre
     end
   end
