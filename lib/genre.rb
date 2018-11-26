ea148a3bcfda37b1901d9608d05d5c6f2a013853
Brandon Teich <brandonteich123@gmail.com>
2018-11-26 06:44:34 +0000

    automatically backed up by learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,20 +1,18 @@
 class Genre
   attr_accessor :name, :songs
-  
   def initialize(name)
       @name = name
       @songs = []
   end
-  
   def add_song(song)
       @songs << song
   end
-  
   def songs
       @songs
   end
-  
   def artists
-      @songs.collect { |song| song.artist }
+      @songs.collect do |song| 
+        song.artist 
+      end
   end
 end
